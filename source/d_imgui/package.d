70b7303f8a3b7a8ba4036182ebec19efc7cca841
KytoDragon <kytodragon@e.mail.de>
2020-10-11 17:58:47 +0200

    remove github workflow, added dub.json, added description to README

---
+++ source/d_imgui/package.d
@@ -0,0 +1,9 @@
+module d_imgui;
+
+public import d_imgui.imconfig;
+public import d_imgui.imgui_h;
+public import d_imgui.imgui;
+public import d_imgui.imgui_widgets;
+public import d_imgui.imgui_internal;
+public import d_imgui.imgui_draw; 
+public import d_imgui.imgui_demo;
