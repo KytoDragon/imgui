5a4cb61eb0aa0db44a6902df48da89de46a6b5ce
KytoDragon <kytodragon@e.mail.de>
2024-06-27 22:46:52 +0200

    fix disabling gamepad integration on the win32 backend

--- backends_d/imgui_impl_win32.d
+++ backends_d/imgui_impl_win32.d
@@ -605,7 +605,9 @@ LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARA
         if (msg == WM_LBUTTONDOWN || msg == WM_LBUTTONDBLCLK) { button = 0; }
         if (msg == WM_RBUTTONDOWN || msg == WM_RBUTTONDBLCLK) { button = 1; }
         if (msg == WM_MBUTTONDOWN || msg == WM_MBUTTONDBLCLK) { button = 2; }
+        version (IMGUI_IMPL_WIN32_DISABLE_GAMEPAD) {} else {
         if (msg == WM_XBUTTONDOWN || msg == WM_XBUTTONDBLCLK) { button = (GET_XBUTTON_WPARAM(wParam) == XBUTTON1) ? 3 : 4; }
+        }
         if (bd.MouseButtonsDown == 0 && GetCapture() == null)
             SetCapture(hwnd);
         bd.MouseButtonsDown |= 1 << button;
@@ -623,7 +625,9 @@ LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARA
         if (msg == WM_LBUTTONUP) { button = 0; }
         if (msg == WM_RBUTTONUP) { button = 1; }
         if (msg == WM_MBUTTONUP) { button = 2; }
+        version (IMGUI_IMPL_WIN32_DISABLE_GAMEPAD) {} else {
         if (msg == WM_XBUTTONUP) { button = (GET_XBUTTON_WPARAM(wParam) == XBUTTON1) ? 3 : 4; }
+        }
         bd.MouseButtonsDown &= ~(1 << button);
         if (bd.MouseButtonsDown == 0 && GetCapture() == hwnd)
             ReleaseCapture();
